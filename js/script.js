9222b7c2765cbe34ff8a89ca87c042a10c4e855b
CloudPeptides <jessica.holsopple3@gmail.com>
2026-07-07 14:32:02 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
+const mobileToggle = document.querySelector(".mobile-toggle");
+const navLinks = document.querySelector(".nav-links");
 
+if (mobileToggle && navLinks) {
+  mobileToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("open");
+  });
+}
