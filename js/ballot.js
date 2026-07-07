fc0ca8ab9156e8af33a34ae76ef0587c82f50b07
CloudPeptides <jessica.holsopple3@gmail.com>
2026-07-07 19:25:33 +0200

    Update ballot.js

--- js/ballot.js
+++ js/ballot.js
@@ -1,1 +1,14 @@
+async function loadBallot() {
 
+    const { data, error } = await supabase
+        .from("ballot_options")
+        .select("*")
+        .eq("active", true)
+        .order("category")
+        .order("name");
+
+    console.log(data);
+
+}
+
+loadBallot();
