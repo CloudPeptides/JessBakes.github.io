e6252cfd8c947e4d830afb6deccbd454b9ef855e
CloudPeptides <jessica.holsopple3@gmail.com>
2026-07-07 19:29:12 +0200

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,7 +1,7 @@
 const SUPABASE_URL = "https://fbfvqiuhwqfhhxufgmla.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_bVAGi0NZtzqmICQ_RJ79IQ_Lvz-UVEb";
 
-const supabase = window.supabase.createClient(
+const supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_ANON_KEY
 );
