f482b068206d7e98307ce81d4f136ea87b3a2530
CloudPeptides <jessica.holsopple3@gmail.com>
2026-07-13 22:21:24 +0200

    Update admin-analytics.js

--- js/admin-analytics.js
+++ js/admin-analytics.js
@@ -4,6 +4,17 @@ document.addEventListener("DOMContentLoaded", async () => {
 
 });
 
+function escapeHtml(text) {
+
+    return String(text ?? "")
+        .replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;")
+        .replace(/"/g, "&quot;")
+        .replace(/'/g, "&#039;");
+
+}
+
 /* ==========================================
    PAGE INITIALIZATION
 ========================================== */
